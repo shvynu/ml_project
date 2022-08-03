3bb490d29e02ea44477de658161aca96157bb7df
Anurag sikdar <90986145+shvynu@users.noreply.github.com>
2022-08-03 20:19:26 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# ml_project
-face recongintion system
+![](https://www.itsguru.com/wp-content/uploads/2020/02/Facial-Recognition.jpg "Attendance using Face Recognition")
+# Attendance using Face Recognition
+This project is made to take attendance in organisation(s). This uses face recognition to check a person's identity and mark him/her present or absent accordingly.
+
+## Project Description
+This is an **AI based project** working on the principle of **Computer Vision**. <br>
+Using digital images and live feed it detects and **recognizes human face** and **mark their attendance**. <br>
+The sole purpose of this model is to detect a person and add their details separately in Attendance list. <br>
+
+## Novelty
+- Instead of working on a single face, our model can **recognize multiple face in one frame**.
+- Our model not only recognizes a face but also **mark attendance** of that recognized person.
+- Our model update Attendance list **in real-time** (as soon as it recognizes a person).
+
+## Real-time Usage
+- Firstly, we **open the webcam** to take images of people present in front of the camera.
+- Then we **check** each of them if they exist in our data or not. If not, they'll be called as 'Unknown'.
+- After checking each of them with our data, we check that if they already **marked as present** or not. If not, we take that person's Registration no., Name & Entry time and mark them present.
+
+## Hardware & Software Requirements
+1. **Hardware**: Desktop or laptop with a webcam installed
+2. **Minimum Specs**: 4gb RAM and 80gb HDD dual core processor
+3. **OS**: macOS or Linux (Windows not officially support 'face_ recognition' library, but it might work)
+4. **Programming Language**: Python 2.7 or Python 3.3+
+5. **Application**: 'Spreadsheet' in macOS or 'LibreOffice Calc' in Linux
+
+## Output
+|Registration_No.|Name|Entry_Time|
+|---|---|---|
+|19BCE10191|Anurag|09:10:39|
+|19BCE10118|Gaurav|09:12:15|
+
+## Result & Discussion
+- This method can **detect multiple face** in one frame and can be easily used in a classroom or in an office.
+- This system helps us to achieve desired results with **better accuracy** and less time consumption.
+- The precision or the accuracy of face recognition of our model is almost **more than 90%**.
+
+## Conclusion
+Thus, the aim of this model is to capture the video of the
+students/colleagues, convert it into frames, relate it with the dataset to
+ensure their presence or absence, mark attendance to the
+particular student/colleagues to maintain the record. The Automated
+Classroom Attendance System helps in increasing the
+accuracy and speed ultimately achieve the high-precision
+real-time attendance to meet the need for automatic
+classroom evaluation.
+
+## Steps to Run
+1. Fork this repo
+2. Clone the forked repo to your local system
+3. Install the following libraries: (in **Linux** or **macOS**)
+   1. `cv2`
+   2. `face_recogniton`
+   3. `os`
+   4. `math`
+   5. `numpy`
+   6. `datetime`
+4. Add your image inside `imageData` folder in **format** -> `name.registration.jpg`, if adding more than one image of same person then **format** -> `name.registration.(0,1,2).jpg`, only after that it will recognize you. 
+5. Run the code -> `code.py`
+6. If it will recognise you, your attendance will be there in `Attendance.csv` file
+
+**(Provide more than one image with different angle to get more accuracy)**
+
---
+++ code.py
@@ -0,0 +1,101 @@
+# THIS IS THE FINAL CODE FOR VIDEO
+
+from cv2 import cv2
+import face_recognition
+import os
+import math
+import numpy
+from datetime import datetime
+
+videoCapture = cv2.VideoCapture(0)
+
+# FOR MARKING ATTENDANCE
+def markAttendance(regNumber, name):
+    f = open("Attendance.csv", "r+")
+    data = f.readlines()
+    regNumberList = []
+    for line in data:
+        entry = line.split(',')
+        regNumberList.append(entry[0])
+    if regNumber not in regNumberList:
+        time = datetime.now()
+        # timeFormat = time.strftime('%H:%M:%S')
+        f.writelines(f'\n{regNumber},{name},{time}')
+
+# FOR CHECKING THE ACCURACY
+def getAccuracy(faceDistance, faceMatchThreshold = 0.6):
+    if faceDistance > faceMatchThreshold:
+        range = (1.0 - faceMatchThreshold)
+        linearValue = (1.0 - faceDistance) / (range * 2.0)
+        return linearValue
+    else:
+        range = faceMatchThreshold
+        linearValue = 1.0 - (faceDistance / (range * 2.0))
+        return linearValue + ((1.0 - linearValue) * math.pow((linearValue - 0.5) * 2, 0.2))
+
+# FOR GETTING PATH, NAME, REGISTRATION NO. AND ENCODINGS OF EACH PERSON
+allPaths = os.listdir("imageData")
+allNames = []
+allRegNumbers = []
+allEncodings = []
+for index in range(len(allPaths)):
+    allNames.append(allPaths[index].split(".")[0])
+    allRegNumbers.append(allPaths[index].split(".")[1])
+    image = face_recognition.load_image_file("imageData/" + allPaths[index])
+    temp = face_recognition.face_encodings(image)[0]
+    allEncodings.append(temp)
+
+while True:
+    ret, frame = videoCapture.read()
+
+    frame = cv2.resize(frame, (0, 0), fx=2, fy=1.6)
+
+    resizedFrame = cv2.resize(frame, (0, 0), fx=0.2, fy=0.2)
+
+    requiredFrame = cv2.cvtColor(resizedFrame, cv2.COLOR_BGR2RGB)
+
+    faceLocation = face_recognition.face_locations(requiredFrame)
+
+    faceEncoding = face_recognition.face_encodings(requiredFrame, faceLocation)
+
+    faceNames = []
+    for encoding in faceEncoding:
+
+        ismatched = face_recognition.compare_faces(allEncodings, encoding)
+        matchedName = "Unknown"
+
+        faceDistance = face_recognition.face_distance(allEncodings, encoding)
+
+        if faceDistance[0] > faceDistance[1]: minimumFaceDistance = faceDistance[1]
+        else: minimumFaceDistance = faceDistance[0]
+
+        accuracy = getAccuracy(minimumFaceDistance)*100
+
+        bestMatchIndex = numpy.argmin(faceDistance)
+
+        #faceCoordinates = list(i*5 for i in faceLocation[0])
+
+        if ismatched[bestMatchIndex] and accuracy > 70:
+            matchedName = allNames[bestMatchIndex]
+            markAttendance(allRegNumbers[bestMatchIndex], matchedName)
+            #cv2.putText(frame, "%.2f"%accuracy + "%", (faceCoordinates[3] + 6, faceCoordinates[2]+ 30), cv2.FONT_HERSHEY_DUPLEX, 1.0, (0, 255, 0), 2)
+
+        faceNames.append(matchedName)
+
+    for (top, right, bottom, left), name in zip(faceLocation, faceNames):
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 3)
+        cv2.putText(frame, name, (left + 6, bottom - 10), cv2.FONT_HERSHEY_DUPLEX, 1.5, (0, 255, 0), 2)
+        if(accuracy > 70):
+            cv2.putText(frame, "%.2f"%accuracy + "%", (left + 6, bottom + 30), cv2.FONT_HERSHEY_DUPLEX, 1.0, (0, 255, 0), 2)
+        
+
+        #cv2.rectangle(frame, (faceCoordinates[3], faceCoordinates[0]), (faceCoordinates[1], faceCoordinates[2]), (0, 255, 0), 3)
+        #cv2.putText(frame, matchedName, (faceCoordinates[3] + 6, faceCoordinates[2] - 10), cv2.FONT_HERSHEY_DUPLEX, 1.5, (0, 255, 0), 2)
+        
+    cv2.imshow("Recording video", frame)
+    cv2.waitKey(1)
